4d0e897546548c37b545845a9ee6c8563f981fb3
Thanh.ngm <thanh.nguyenminh@watasolutions.com>
2021-09-03 14:26:34 +0700

    home-product-detail-screen

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
+
+const App = () => {
+  return (
+    <Router>
+      <Header></Header>
+      <Container>
+        <main className='py-3'>
+          <Route path='/' component={HomeScreen} exact></Route>
+          <Route path='/product/:id' component={ProductScreen} ></Route>
+        </main>
+      </Container>
+
+      <Footer></Footer>
+    </Router>
+  );
+
+}
+
+export default App;
